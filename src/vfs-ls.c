03c0b418cb32c3a500c4ed51d3d2c1503c3af19f
SDeGiuda <santiagodegiuda@gmail.com>
2025-06-21 11:39:09 -0300

    sa

--- src/vfs-ls.c
+++ src/vfs-ls.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
     }
 
 
-    for (int i = 0; i < num_entries; i++) {
+    for (int i = 1; i < num_entries; i++) {
         struct inode file_inode;
 
         if (read_inode(image_path, entries[i].inode, &file_inode) != 0) {
